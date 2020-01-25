1e7b0d3af04314adf7e2d7aac55234a3da39bd38
Aissata Keita <35870800+Aissatakeita@users.noreply.github.com>
2020-01-25 15:45:42 +0100

    Update allume_led.ino

--- src/allume_led.ino
+++ src/allume_led.ino
@@ -4,13 +4,14 @@ int led = 13;
 void setup() {
   
   // put your setup code here, to run once:
-  pinMode(led, OUTPUT);
+  pinMode(led, OUTPUT); //Signale à l’Arduino que la connexion 13 doit pouvoir envoyer du courant
 
 }
 
 void loop() {
-    digitalWrite(led,HIGH);
-    delay(1000);
+  
+    digitalWrite(led,HIGH);// Demande à l’Arduino d’envoyer du courant dans la connexion 13
+    delay(1000);// le temps d'allumage 
     digitalWrite(led,LOW);
     delay(1000);
     
