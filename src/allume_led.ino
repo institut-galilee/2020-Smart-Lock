458e2c81f3e024c6ad83cd8933cb37cae4bcb191
Aissata Keita <35870800+Aissatakeita@users.noreply.github.com>
2020-01-25 15:40:07 +0100

    Add files via upload

---
+++ src/allume_led.ino
@@ -0,0 +1,20 @@
+
+
+int led = 13;
+void setup() {
+  
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+    digitalWrite(led,HIGH);
+    delay(1000);
+    digitalWrite(led,LOW);
+    delay(1000);
+    
+
+  // put your main code here, to run repeatedly:
+
+}
