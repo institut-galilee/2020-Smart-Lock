e7f7471384ef967a375ed0a51253bbf1eea6e951
MichelYABA <46414213+MichelYABA@users.noreply.github.com>
2020-02-13 23:35:14 +0100

    modification code

--- lab/2/code1.png.ino
+++ lab/2/code1.png.ino
@@ -9,41 +9,25 @@ void setup() {
 void loop() {
   // if there's any serial available, read it:
   if (Serial.available() > 0) {
-
-    // look for the next valid integer in the incoming serial stream:
-    //int i  = Serial.parseInt();
-    // look for the next valid byte in the incoming serial stream:
+    
     char buzzer_picked = Serial.read();
 
     int buzzer_freq  = Serial.parseInt();
 
-    if (Serial.read()=='\n'){
-      tone(buz, buzzer_freq); 
-      //Serial.println(buzzer_freq); 
-      Serial.println(buzzer_freq, HEX);
-      Serial.println("la fréquence a changé");
-    }
-
-    //write_freq(buzzer_picked, buzzer_freq);
+    afficher_frequence(buzzer_picked, buzzer_freq);
        
   }
 
 }
 
-void write_freq(char buzzer, int frequency)
+void afficher_frequence(char buzzer, int frequency)
 {
-    /*if (buzzer == 'f')
-    {*/
-        tone(buz, frequency);
-        /*Serial.println(buzzer, DEC);*/
-        Serial.println("Nouvelle fréquence : ");
-        Serial.println(frequency);
-
-        //return;
-   /* }else{
-      tone(buz, 450);
-      return;
-    }*/
+    if (Serial.read()=='\n'){
+      tone(buz, frequency); 
+      //Serial.println(buzzer_freq); 
+      Serial.println(frequency, HEX);
+      Serial.println("la fréquence a changé");
+    }
     
     return;
 }
